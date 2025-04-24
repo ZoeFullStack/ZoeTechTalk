a7b82c72db10dd82810cc4fc27120c4835f252ae
zoe <kbl130991@hotmail.com>
2025-04-24 19:10:19 +0900

    chore(search): make sure only can search in post page

--- _scripts/search.liquid.js
+++ _scripts/search.liquid.js
@@ -6,52 +6,33 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [
-  {%- for page in site.pages -%}
-    {%- if page.permalink == '/' -%}{%- assign about_title = page.title | strip -%}{%- endif -%}
-  {%- endfor -%}
-  {
-    id: "nav-{{ about_title | slugify }}",
-    title: "{{ about_title | truncatewords: 13 }}",
-    section: "Navigation",
-    handler: () => {
-      window.location.href = "{{ '/' | relative_url }}";
-    },
+/*
+{%- for page in site.pages -%}
+  {%- if page.permalink == '/' -%}{%- assign about_title = page.title | strip -%}{%- endif -%}
+{%- endfor -%}
+{
+  id: "nav-{{ about_title | slugify }}",
+  title: "{{ about_title | truncatewords: 13 }}",
+  section: "Navigation",
+  handler: () => {
+    window.location.href = "{{ '/' | relative_url }}";
   },
-  {%- assign sorted_pages = site.pages | sort: "nav_order" -%}
-  {%- for p in sorted_pages -%}
-    {%- if p.nav and p.autogen == null -%}
-      {%- if p.dropdown -%}
-        {%- for child in p.children -%}
-          {%- unless child.title == 'divider' -%}
-            {
-              {%- assign title = child.title | escape | strip -%}
-              {%- if child.permalink contains "/blog/" -%}{%- assign url = "/blog/" -%} {%- else -%}{%- assign url = child.url -%}{%- endif -%}
-              id: "dropdown-{{ title | slugify }}",
-              title: "{{ title | truncatewords: 13 }}",
-              description: "{{ child.description | strip_html | strip_newlines | escape | strip }}",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "{{ url | relative_url }}";
-              },
-            },
-          {%- endunless -%}
-        {%- endfor -%}
-
-      {%- else -%}
-        {
-          {%- assign title = p.title | escape | strip -%}
-          {%- if p.permalink contains "/blog/" -%}{%- assign url = "/blog/" -%} {%- else -%}{%- assign url = p.url -%}{%- endif -%}
-          id: "nav-{{ title | slugify }}",
-          title: "{{ title | truncatewords: 13 }}",
-          description: "{{ p.description | strip_html | strip_newlines | escape | strip }}",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "{{ url | relative_url }}";
-          },
-        },
-      {%- endif -%}
-    {%- endif -%}
-  {%- endfor -%}
+},
+{%- assign sorted_pages = site.pages | sort: "nav_order" -%}
+{%- for p in sorted_pages -%}
+  {%- if p.nav and p.autogen == null -%}
+    {
+      id: "nav-{{ p.title | slugify }}",
+      title: "{{ p.title | truncatewords: 13 }}",
+      description: "{{ p.description | strip_html | strip_newlines | escape | strip }}",
+      section: "Navigation",
+      handler: () => {
+        window.location.href = "{{ p.url | relative_url }}";
+      },
+    },
+  {%- endif -%}
+{%- endfor -%}
+*/
   {%- for post in site.posts -%}
     {
       {%- assign title = post.title | escape | strip -%}
@@ -76,7 +57,8 @@ ninja.data = [
       },
     },
   {%- endfor -%}
-  {%- for collection in site.collections -%}
+  /** 
+    {%- for collection in site.collections -%}
     {%- if collection.label != 'posts' -%}
       {%- for item in collection.docs -%}
         {
@@ -324,4 +306,5 @@ ninja.data = [
       },
     },
   {%- endif -%}
+  */
 ];
